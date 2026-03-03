796dd546c949b3ed8b0bc585e3d927d16c5ce0b5
yvr-teja <yvrteja@outlook.com>
2026-03-03 08:05:34 +0530

    Day two of Stream practice completed.

--- src/main/java/org/java8andlater/StreamsAPI/CollectorsSummingDouble/StreamsDemo2.java
+++ src/main/java/org/java8andlater/StreamsAPI/CollectorsSummingDouble/StreamsDemo2.java
@@ -1,6 +1,7 @@
 package org.java8andlater.StreamsAPI.CollectorsSummingDouble;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamsDemo2 {
     public static void main(String[] args) {
@@ -14,12 +15,36 @@ public class StreamsDemo2 {
                 new Employee(7,"Janani", "HR", 50000,List.of("SocialMedia", "Lead")),
                 new Employee(8,"Kavya", "IT", 80000,List.of("Java", "Spring", "React"))
         );
-    //
+    //Collectors.averagingDouble
     //  Average salary of all employees
-    //  Average salary of IT dept
+        double avgAllEmps = employees.stream()
+                //.collect(Collectors.averagingDouble(emp1->emp1.getSalary()));
+                .collect(Collectors.averagingDouble(Employee::getSalary));
+        System.out.println("Average salary of All employee: " + avgAllEmps);
+        //  Average salary of Marketing dept
+        double avgItEmps = employees.stream()
+                .filter(employee -> employee.getDepartment().equals("Marketing"))
+                //.collect(Collectors.averagingDouble(emp1->emp1.getSalary()));
+                .collect(Collectors.averagingDouble(Employee::getSalary));
+        System.out.println("Average salary of IT employee: " + avgItEmps);
     //  Average salary of employees earning more than 80k
-    //  Average bonus of employees
+        double highAvgEmps = employees.stream()
+                .filter(emp -> emp.getSalary() > 80000)
+                //.collect(Collectors.averagingDouble(emp1->emp1.getSalary()));
+                .collect(Collectors.averagingDouble(Employee::getSalary));
+        System.out.println("Average salary of All employee: " + highAvgEmps);
+    //  Average bonus of employees(Bonus is 10% salary)
+        double avgBonus = employees.stream()
+                .collect(Collectors.averagingDouble(emp1->emp1.getSalary()*0.10));
+        System.out.println("Average salary of All employee: " + avgBonus);
     //  Average number of skills per employee
-    //  Average training cost per employee
+        double avgSkillEmps = employees.stream()
+                .collect(Collectors.averagingDouble(emp-> emp.getSkills().size()));
+        System.out.println(avgSkillEmps);
+    //  Average training cost per employee(Cost per skill is 2k)
+        double avgTraningCost = employees.stream()
+                .collect(Collectors.averagingDouble(emp1->emp1.getSkills().size()*2000));
+
+        System.out.println(avgTraningCost);
     }
 }
